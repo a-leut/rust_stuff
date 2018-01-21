830da014eb37b601db77f45aa5cadff42d0cde7c
aleut <turbomonkeyninja@gmail.com>
2018-01-20 22:47:54 -0700

    added prime gen function

--- project_euler/p3/src/main.rs
+++ project_euler/p3/src/main.rs
@@ -1,3 +1,25 @@
+fn primes(max_len: usize) -> Vec<usize> {
+    // Sieve of Eratosthenes 
+    let mut can = vec![1; max_len];  // 1 is prime
+    for i in 2..max_len {
+        // If i is prime
+        if can[i] == 1 {
+            // Set multiples of i to not prime
+            for step in 2..((max_len / i) + 1) {
+                can[(step * i) - 1] = 0;
+            }
+        }
+    }
+    let mut primes = Vec::new();
+    for i in 1..max_len { 
+        if can[i] == 1 {
+            primes.push(i + 1);
+        }
+    }
+    return primes.to_vec();
+}
+
 fn main() {
-    let v: Vec<i32> = Vec::new();
+    let p = primes(50);
+    println!("primes: {:?}", p);
 }
---
+++ project_euler/p3/target/debug/p3.d
@@ -0,0 +1,1 @@
+C:\dev\rust_stuff\project_euler\p3\target\debug\p3.exe: C:\dev\rust_stuff\project_euler\p3\src\main.rs
