0d92b371d90fe44fe348659d280022025b7fd405
aleut <turbomonkeyninja@gmail.com>
2018-01-20 22:05:29 -0700

    i dunno

--- project_euler/p27/src/main.rs
+++ project_euler/p27/src/main.rs
@@ -1,20 +1,19 @@
-fn gen_primes(limit: u32) {
+/*fn gen_primes(limit: u32) {
 	let isp [bool; limit] = [true; limit];
 	for n in isp {
 
 	}
-	isp
+	isp.map(|)
 	return p;
 }
 
 fn max_prime_seq(a: i32, b: i32) -> i32 {
 	32
-}
+}*/
 
 fn main() {
-	let primes = gen_primes(10000);
-    for a in (-999..1000) {
-		for b in (-1000..1001) {
-		}
+	//let primes = gen_primes(10000);
+    for a, b in (-999..1000).zip((-1000..1001)) {
+		println!("{} {}", a, b);
 	}
 }
